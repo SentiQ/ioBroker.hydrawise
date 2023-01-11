6791eed58a586b422a94b1908747b31d83ad6ed6
ioBroker Adapter Creator <noreply@iobroker.net>
2023-01-11 09:18:08 +0000

    Initial commit by adapter creator

---
+++ src/lib/adapter-config.d.ts
@@ -0,0 +1,13 @@
+// This file extends the AdapterConfig type from "@types/iobroker"
+
+// Augment the globally declared type ioBroker.AdapterConfig
+declare global {
+	namespace ioBroker {
+		interface AdapterConfig {
+			apiKey: string;
+		}
+	}
+}
+
+// this is required so the above AdapterConfig is found by TypeScript / type checking
+export {};
